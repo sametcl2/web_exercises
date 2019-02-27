d424ead2ec60cf1f88ce1156aa522ce909ea0cb0
sametcl2 <34509901+sametcl2@users.noreply.github.com>
2019-02-27 19:38:42 +0300

    final

--- Bookmarker/app.js
+++ Bookmarker/app.js
@@ -4,13 +4,25 @@ function bookmark(){
     var siteName=document.getElementById('inputName').value;
     var siteUrl=document.getElementById('inputUrl').value;
 
+    if(!siteName || !siteUrl){
+        alert("Hatalı giriş");
+        return false;
+    }
+    var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
+
+    if(!siteUrl.match(regex)){
+        alert("Hatalı giriş")
+        return false;
+    }
+
     //Object oluşturma
     var bookmark={
         name:siteName,
         url:siteUrl
     }
 
-    //Json'a gönderme. Array'a attım. Hepsini depolamak için. Depolayacağım için JSON'a dönüştürdüm.
+    //Json'a gönderme. Array'a attım hepsini depolamak için. Depolayacağım için JSON'a dönüştürdüm.
     if(localStorage.getItem('bookmarks')===null){
         var bookmarks=[];
         bookmarks.push(bookmark);
@@ -20,21 +32,42 @@ function bookmark(){
         bookmarks.push(bookmark);
         localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
     }
+
+    localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+    fetch();
+}
+
+function deletee(url){
+    var bookmarks=JSON.parse(localStorage.getItem('bookmarks'));
+    for(var i=0; i<bookmarks.length;i++){
+        if(bookmarks[i].url==url){
+            bookmarks.splice(i,1);  
+        }
+    }
+    localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+    fetch();
 }
 
 function fetch(){
     var bookmarks=JSON.parse(localStorage.getItem('bookmarks'));
     var results=document.getElementById("bookmarkResults");
 
+    results.innerHTML="";
     for(var i=0; i<bookmarks.length; i++){
+       
         var name=bookmarks[i].name;
         var url=bookmarks[i].url;
 
-        results.innerHTML+='<div class=" p-4 d-flex ">'+
-                            '<h3 class="mx-5">'+name+
-                            '</h3>'+
-                            '<a class="btn btn-primary" href="'+url+'">Visit</a>'+                        
-                            '</div>';
+        results.innerHTML+='<div class="d-flex justify-content-between ">'+
+                            '<div class=" p-4">'+
+                            '<h3>'+name+ '</h3>'+  
+                            '</div>'+
+                            '<div class=" p-4">'+
+                            '<a class="btn btn-primary mx-3 " href="'+url+'">Visit</a>'+ 
+                            '<a class="btn btn-danger mx-3" onclick="deletee(\''+url+'\')" href="#">Delete</a>'+ 
+                            '</div>'+
+                            '</div>'+
+                            '<hr>';
     }
 }
 
