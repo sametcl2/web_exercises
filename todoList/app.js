692f77345f43f21411eff7cf7bda8fe9904f5223
sametcl2 <34509901+sametcl2@users.noreply.github.com>
2019-03-04 20:45:52 +0300

    final

--- todoList/app.js
+++ todoList/app.js
@@ -24,15 +24,49 @@ function ekle(gorev){
     show();
 }
 
-function deletee(isim){
+function switchh(isim){
     var sill=JSON.parse(localStorage.getItem("gorev"));
     for(var i=0; i<sill.length; i++){
        if(sill[i].gorev==isim){
             sill.splice(i,1);  
+            localStorage.setItem("gorev",JSON.stringify(sill));
        }
     }
-    localStorage.setItem("gorev",JSON.stringify(sill));
+    if((localStorage.getItem("done")==null)){
+        var done=[];
+        done.push(isim);
+        localStorage.setItem("yapildi",JSON.stringify(done));
+    }else{
+        var done=JSON.parse("yapildi");
+        done.push(isim);
+        localStorage.setItem("yapildi",JSON.stringify(done));
+    }
     show();
+    bitti();
+}
+
+function bitti(){
+    var ikincideger=JSON.parse(localStorage.getItem("yapildi"));
+    var result=document.getElementById("bitti");
+    result.innerHTML="";
+    for(var i=0; i<ikincideger.length;i++){
+        var ikinciler=ikincideger[i];
+        result.innerHTML+='<li class="list-group-item d-flex justify-content-between m-2 p-4">'+
+        '<h4 class="align-middle">'+ikinciler+'</h4>'+
+        '<button class="btn btn-danger" id="deletebtn" onclick="sill(\''+ikinciler+'\')">Sil!</button>'+
+        '</li>';
+    }
+}
+
+function sill(silbunu){
+    var tut=JSON.parse(localStorage.getItem("yapildi"));
+    for(var i=0; i<tut.length;i++){
+        if(tut[i]==silbunu){
+            tut.splice(i,1);
+            localStorage.setItem("yapildi",JSON.stringify(tut)); 
+        }
+    }
+    bitti();
 }
 
 function show(){
@@ -43,8 +77,7 @@ function show(){
         var yeniGorev=gorevs[i].gorev;
         result.innerHTML+='<li class="list-group-item d-flex justify-content-between m-2 p-4">'+
                           '<h4 class="align-middle">'+yeniGorev+'</h4>'+
-                          '<button class="btn btn-primary" id="sil" onclick="deletee(\''+yeniGorev+'\')">Done!</button>'+
-                          '</hr>'+
+                          '<button class="btn btn-primary" id="sil" onclick="switchh(\''+yeniGorev+'\')">Done!</button>'+
                           '</li>';
     }
 }
